a4f4a3fae2c44aa3909571ebf9d8d1b51ba3dd92
Michael Long <michael.long35@gmail.com>
2015-04-15 16:41:01 +0100

    Initial Commit, version 1.0.6

---
+++ resources/dynamic/standard_multiple.js
@@ -0,0 +1,16 @@
+﻿/* standard_multiple.js */
+{% 
+Dim i 
+Dim ar = CurrentQuestion.AvailableResponses
+Dim inputName = CurrentQuestion.InputName()
+Dim inputValue
+Dim entryCode
+Dim isExclusive
+
+For i = 1 To ar.Count 
+    isExclusive = ar[i].IsExclusive
+	inputValue = ar[i].InputValue()
+	entryCode = ar[i].EntryCodeStr
+%}
+{entryCode : "{%= entryCode %}", inputValue : "{%= inputValue %}", element : $("input[name='{%= CurrentQuestion.InputName("List")%}']"), isExclusive : {%= isExclusive%}}{%= On(i < ar.Count, ",", "") %}
+{% Next %}
---
+++ resources/dynamic/standard_single.js
@@ -0,0 +1,17 @@
+﻿/* standard_single.js */
+{% 
+Dim i 
+Dim ar = CurrentQuestion.AvailableResponses
+Dim inputName
+Dim inputValue
+Dim caption
+Dim entryCode
+
+For i = 1 To ar.Count 
+	inputName = ar[i].InputName()
+	inputValue = ar[i].InputValue()
+	caption   = ar[i].Caption
+	entryCode   = ar[i].EntryCodeStr
+%}
+{caption : "{%= caption %}", entryCode : "{%= entryCode %}", inputValue : "{%= inputValue %}", element : $('#{%= inputName%}')}{%= On(i < ar.Count, ",", "") %}
+{% Next %}
